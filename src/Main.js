83df5e11a7c736ff578584f9055ff9783215a769
dearaugust <419491539@qq.com>
2017-05-27 09:12:46 +0800

    add react todo

---
+++ src/Main.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import TodoList from './TodoList.js'
+
+const Header = () =>(
+  <div className="header">TODO</div>
+)
+
+const Form = () =>(
+  <div className="form">Form</div>
+)
+
+const Actions = () =>(
+  <div className="actions">Actions</div>
+)
+
+class Main extends Component {
+  render(){
+    return(
+      <div className="main">
+        <Header />
+        <TodoList />
+        <Form />
+        <Actions />
+      </div>
+    )
+  }
+}
+
+export default Main
---
+++ src/TodoList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class TodoList extends Component {
+  render(){
+    return(
+      <div className="todo-list">
+        <div className="contarner">
+          <ul>
+            <li>Todo</li>
+            <li>Todo</li>
+            <li>Todo</li>
+            <li>Todo</li>
+            <li>Todo</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TodoList
